429e57b34b281d97de4b962b5baada5ef46bbf99
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-09-04 13:15:33 +1200

    Updated

--- UnitedYouthOrchestra7/Pages/Policy.cshtml.cs
+++ UnitedYouthOrchestra7/Pages/Policy.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace UnitedYouthOrchestra7.Pages
+namespace UnitedYouthOrchestra7.Pages.Policy
 {
     public class PrivacyModel : PageModel
     {
